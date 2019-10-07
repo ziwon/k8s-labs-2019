dcfc430989c52fe8350847cc79916f6045814a93
Youngpil Yoon <yngpil.yoon@gmail.com>
2019-10-08 02:04:13 +0900

    fix indent

--- klab.sh
+++ klab.sh
@@ -21,31 +21,30 @@ init() {
   echo ">> Waiting for cluster to get ready..."
   sleep 10
 
-
   KUBECONFIG="$(k3d get-kubeconfig --name="${CLUSTER_NAME}")"
   export KUBECONFIG
 
-    # Install local-path-storage
-    kubectl --kubeconfig="${KUBECONFIG}" apply -f "addons/local-path-storage.yaml"
-    kubectl --kubeconfig="${KUBECONFIG}" patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+  # Install local-path-storage
+  kubectl --kubeconfig="${KUBECONFIG}" apply -f "addons/local-path-storage.yaml"
+  kubectl --kubeconfig="${KUBECONFIG}" patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
-    # Install cert-manager
-    kubectl --kubeconfig="${KUBECONFIG}" apply -f "https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml"
-    kubectl --kubeconfig="${KUBECONFIG}" apply -f "addons/cert-manager.yaml"
+  # Install cert-manager
+  kubectl --kubeconfig="${KUBECONFIG}" apply -f "https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml"
+  kubectl --kubeconfig="${KUBECONFIG}" apply -f "addons/cert-manager.yaml"
 
-    # Install cluster issuers
-    kubectl --kubeconfig="${KUBECONFIG}" apply -f "issuers/cluster-issuer-prod.yaml"
-    kubectl --kubeconfig="${KUBECONFIG}" apply -f "issuers/cluster-issuer-staging.yaml"
+  # Install cluster issuers
+  kubectl --kubeconfig="${KUBECONFIG}" apply -f "issuers/cluster-issuer-prod.yaml"
+  kubectl --kubeconfig="${KUBECONFIG}" apply -f "issuers/cluster-issuer-staging.yaml"
 
-    # Install metrics
-    git clone https://github.com/kubernetes-incubator/metrics-server.git
-    kubectl apply -f metrics-server/deploy/1.8+/
-    rm -rf metrics-server
+  # Install metrics
+  git clone https://github.com/kubernetes-incubator/metrics-server.git
+  kubectl apply -f metrics-server/deploy/1.8+/
+  rm -rf metrics-server
 
-    echo ">> Your k3s cluster ready"
+  echo ">> Your k3s cluster ready"
 
-    exit 0
-  }
+  exit 0
+}
 
 up() {
   k3d start --name ${CLUSTER_NAME}
